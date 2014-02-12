90b7391adc55cd51e7fe7165c6593f11946a1147
Hackbright Student <user@mini-005.(none)>
2014-02-11 17:52:43 -0800

    WOOH, it makes tuple keys with vlaue pairs. awesome

--- markov.py
+++ markov.py
@@ -15,23 +15,24 @@ def make_chains(corpus):
 
     #split the string a dictionary of bigrams and their frequency value pair
     #create a for loop to loop over our list of words
-    for i in range(len(list_of_words)):
-        
+    for i in range(len(list_of_words)-2):
         # add the word and the next word to the dictionary as a tuple (for the key)
         #in order to do that, we set the wordth item in the dictionary to that key
         
         #you can set a variable to two things (tuple!!)
         # we want the KEY in the dictionary to be the tuple
         # THEN we want the value to be the next word after the tuple
-
+        key_tuple = (list_of_words[i], list_of_words[i+ 1])
 
         #if its in the dictionary then add it to the value (list)
-        if (list_of_words[i], list_of_words[i+ 1]) in dict_of_markov:
+        if dict_of_markov.get(key_tuple):
             #then add the word after it to the value list
+            dict_of_markov[key_tuple].append(list_of_words[i + 2])
+            # set the value to list_of_words[i + 2]
         else:
             #add that shit to the dictionary
         # if not in the dictionary add to the dictionary
-        dict_of_markov[i] = (list_of_words[i], list_of_words[i+ 1])
+            dict_of_markov[key_tuple] = [list_of_words[i + 2]]
         # use the tuple (list[i], list[i + 1]) as a key in the dictionary
         # add the next item in the list [i + 2] as the value (make a list of values)
     #return the dictionary of markov chains we just made with our awesome brains
@@ -57,8 +58,8 @@ def main():
     opened_file.close()
 
     chain_dict = make_chains(input_text)
-    random_text = make_text(chain_dict)
-    print random_text
+    # random_text = make_text(chain_dict)
+    print chain_dict
 
 if __name__ == "__main__":
     main()

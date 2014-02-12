9a82ed1e78cdc1b284bc7d18191e77f4f49b1754
Hackbright Student <user@mini-005.(none)>
2014-02-12 11:07:30 -0800

    Dictionary created, Text Maker outlined in comments

--- markov.py
+++ markov.py
@@ -1,9 +1,5 @@
 import sys 
 
-#find some text
-#attempt creating make chain function. 
-
-
 def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
     markov chains."""
@@ -13,35 +9,44 @@ def make_chains(corpus):
     #take input text and put it in a list
     list_of_words = corpus.split()
 
-    #split the string a dictionary of bigrams and their frequency value pair
     #create a for loop to loop over our list of words
     for i in range(len(list_of_words)-2):
-        # add the word and the next word to the dictionary as a tuple (for the key)
-        #in order to do that, we set the wordth item in the dictionary to that key
-        
-        #you can set a variable to two things (tuple!!)
-        # we want the KEY in the dictionary to be the tuple
-        # THEN we want the value to be the next word after the tuple
+
+         #create a variable that holds the values of adjacent words
         key_tuple = (list_of_words[i], list_of_words[i+ 1])
 
-        #if its in the dictionary then add it to the value (list)
+        #if the key_tuple is in the dictionary then:
         if dict_of_markov.get(key_tuple):
-            #then add the word after it to the value list
+            #append the word after the tuple of words to the value pair
             dict_of_markov[key_tuple].append(list_of_words[i + 2])
-            # set the value to list_of_words[i + 2]
         else:
-            #add that shit to the dictionary
-        # if not in the dictionary add to the dictionary
+            # if the key_tuple is not there, add that shit to the dictionary
+            # and set the value equal to the word following the tuple
             dict_of_markov[key_tuple] = [list_of_words[i + 2]]
-        # use the tuple (list[i], list[i + 1]) as a key in the dictionary
-        # add the next item in the list [i + 2] as the value (make a list of values)
     #return the dictionary of markov chains we just made with our awesome brains
-
     return dict_of_markov
 
 def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
+    # we need the dictionary we made
+    # return a string of random text made from the chains in the dictionary we created
+    # make a string of three words
+    # try using random.choice
+    # maybe use the random number to access a list using their index
+        #but we really want the tuple, so that index(the random number) and the one next to it
+    
+    #make a list
+    #generate a random number 
+    #find the value of that index in the list, and the item next to it (this is the first random tuple to begin our random sentance)
+    #find the matching tuple in the dictionary
+    #use the dictionary to find a random third word after the tuple
+    #use random choice to generate another random number
+    #find the thing at index[random]
+    # add the two strings together
+    # print that shit, yo
+
+
     return "Here's some random text."
 
 def main():
